9ac8e635dfdd501fb4dee4816d3b9d5b71562cfa
Jitendra <Jitendrahappy99@gmail.com>
2024-09-10 22:30:22 +0530

    Add project files.

---
+++ ServiceA/Controllers/AvaluesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AvaluesController : ControllerBase
+    {
+        [HttpGet("getValue")]
+
+        public string Get()
+        {
+            return "this is A Service";
+        }
+    }
+}
---
+++ ServiceB/Controllers/BvaluesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BvaluesController : ControllerBase
+    {
+        [HttpGet("getValue")]
+
+        public string Get()
+        {
+            return "this is B Service";
+        }
+    }
+}
